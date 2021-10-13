482d520a875055fc1f9282499d65226ba5057764
Yogesh Kumar <ykapr17@gmail.com>
2021-10-13 14:41:48 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ const App = () => {
 
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState({})
+    const [order,setOrder] = useState({})
+    const [errorMessage, setErrorMessage] = useState('')
 
     const fetchProducts = async() =>{
         const {data} = await commerce.products.list()
@@ -46,6 +48,25 @@ const handleEmptyCart = async (productId) => {
 }
 
 
+const refreshCart = async()=> {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart)
+}
+
+
+const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+
+    try{
+        const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder)
+    
+    
+        setOrder(incomingOrder);
+        refreshCart();
+    } catch (error){
+                setErrorMessage(error.data.error.message)
+    }
+}
 useEffect(()=>{
    fetchProducts();
    fetchCart();
@@ -69,7 +90,10 @@ useEffect(()=>{
             handleEmptyCart           = {handleEmptyCart} />
             </Route>
             <Route exact path = '/Checkout'>
-                <Checkout/>
+                <Checkout cart = {cart}
+                order = {order}
+                onCaptureCheckout ={handleCaptureCheckout}
+                error = {errorMessage}/>
             </Route>
             </Switch>
         </div>
---
+++ src/components/Checkoutform/Review.js
@@ -0,0 +1,27 @@
+import {Typography, List ,ListItem, ListItemText} from '@material-ui/core'
+
+
+const Review = ({checkoutToken}) => {
+    return (
+        <>
+             <Typography variant = "h6" gutterBottom>order summary</Typography>
+             <List disablePadding>
+               {checkoutToken.live.line_items.map((product)=> (
+                 <ListItem style = {{padding: '10px 0'}} key = {product.name}>
+                   <ListItemText primary = {product.name} secondary ={ `quantity: ${product.quantity}`}/>
+                   <Typography variant = "body2">{product.line_total.formatted_with_symbol}</Typography>
+
+                   </ListItem>
+                   ))}
+                   <ListItem style = {{padding: '10px 0'}}>
+                       <ListItemText primary = "total " />
+                       <Typography variant = 'subtitle1' style = {{fontweight: 700}}>
+                       {checkoutToken.live.subtotal.formatted_with_symbol}
+                       </Typography>
+                   </ListItem>
+             </List>
+        </>
+    )
+}
+
+export default Review
